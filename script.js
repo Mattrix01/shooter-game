ba71ff8303f3a799339fc4fc60a5ce6e8cbef179
Mattrix01 <emjay.knight@gmail.com>
2023-01-17 15:17:34 +0000

    sprite image loaded and flying at correct sizes

--- script.js
+++ script.js
@@ -15,8 +15,14 @@ let ravens = [];
 class Raven {
   // constructor will create one blank object everytime it is called and assign it properties and values as we define them
   constructor() {
-    this.width = 100;
-    this.height = 50;
+    // helper properties for sprite sheet
+    // moved up to calculate values that come after
+    this.spriteWidth = 271;
+    this.spriteHeight = 194;
+    // multiplication is faster than division in code
+    // keep values scaling ratio correct below
+    this.width = this.spriteWidth / 2;
+    this.height = this.spriteHeight / 2;
     // they can fly across to the left so canvas width
     this.x = canvas.width;
     // will be rnaodm number between 0 and canvas height, - height of raven so not off edge of screen
@@ -28,17 +34,38 @@ class Raven {
     // random number beetween -2.5 and +2.5, 1 moves upwards, plus downwards
     this.directionY = Math.random() * 5 - 2.5;
     this.markedForDeletion = false;
+    // loading in raven sprite sheet
+    this.image = new Image();
+    this.image.src = "assets/raven.png";
+    // helper properties for sprite sheet
+    // moved up to calculate values that come after
+    this.spriteWidth = 271;
+    this.spriteHeight = 194;
   }
   // values that ened to be adjusted for moving raven around
   update() {
     this.x -= this.directionX;
     // if horizontal x coordinate of this particular raven object is less than 0 - this.width meaning it has moved behind left edge
     // set markedForDeletion propery as TRUE.
-    if (this.x < 0 - this.width) this, (this.markedForDeletion = true);
+    if (this.x < 0 - this.width) this.markedForDeletion = true;
   }
   // draw method takes updated values and any drawing code will represent single raven object visually
   draw() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+    // callingbuilt in draw image method for ravens, expects between 3 or 9 arguments.
+    // this,width and height scaling entire sprite sheet to fit
+    // 4 arguments after this.image which portion of image we crop. source width height etc.
+    ctx.drawImage(
+      this.image,
+      0,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 // this will create 1 raven object that will have access to update and draw class method
@@ -75,7 +102,7 @@ function animate(timestamp) {
   // can call all at once with below syntax.
   [...ravens].forEach((object) => object.update());
   [...ravens].forEach((object) => object.draw());
-  // use splice while cycling through array to mvoe objects so not using ones going past screen
+  // use splice while cycling through array to move objects so not using ones going past screen
   // use built in array filter method.
   // take var that holds ravens array and reassign to new array. make sure let variable so can be changed.
   // filter method creates a new array
@@ -84,7 +111,6 @@ function animate(timestamp) {
   // take ravens variable and replace with same array but I wasntwed that array to be filled only with objects for which this condition below is TRUE.
   // only objects that ahve marked with deletion property dont go in array.
   ravens = ravens.filter((object) => !object.markedForDeletion);
-
   // using built in below method that will call animate again for constant loop based on timestamps
   requestAnimationFrame(animate);
 }
